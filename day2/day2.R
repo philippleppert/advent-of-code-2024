4fe1a3a137932391d230dfa0ae6955a86389dc1b
Philipp Leppert <philipp-leppert@posteo.de>
2024-12-02 20:57:31 +0100

    Add

--- day2/day2.R
+++ day2/day2.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 day2 <- read_tsv("day2/day2.txt", col_names = F)
 
-sequences_list <- str_split(day2$X1, " ")
+sequences_list_raw <- str_split(day2$X1, " ")
 inc <- list()
 dec <- list()
 diff <- list()
@@ -10,12 +10,29 @@ sum_F_inc <- list()
 sum_F_dec <- list()
 sum_diff <- list()
 for(i in 1:length(sequences_list)){
+
   int_diff <- list()
   inc_i <- list()
   dec_i <- list()
+  sequences_list <- sequences_list_raw
   for(j in 1:length(sequences_list[[i]])-1){
     inc_i[j] <- map2(sequences_list[[i]][j], sequences_list[[i]][j+1], ~ as.numeric(.x) < as.numeric(.y))
+    
+    if(isFALSE(unlist(inc_i[j]))) {
+      sequences_list[[i]][j+1] <- sequences_list[[i]][j]
+    }
+  }
+  sequences_list <- sequences_list_raw
+  for(j in 1:length(sequences_list[[i]])-1){
     dec_i[j] <- map2(sequences_list[[i]][j], sequences_list[[i]][j+1], ~ as.numeric(.x) > as.numeric(.y))
+   
+    if(isFALSE(unlist(dec_i[j]))) {
+      sequences_list[[i]][j+1] <- sequences_list[[i]][j]
+    }
+  }
+  sequences_list <- sequences_list_raw
+  for(j in 1:length(sequences_list[[i]])-1){
+    
     int_diff[j] <- map2(sequences_list[[i]][j], sequences_list[[i]][j+1], ~ abs(as.numeric(.x) - as.numeric(.y)))
   }
   
@@ -56,5 +73,3 @@ result2 <-
       diff_condition == F | (diff_condition == T & diff_fixable == 1)
     )
   )
-
-result2 %>% filter(diff_fixable==1) -> x
